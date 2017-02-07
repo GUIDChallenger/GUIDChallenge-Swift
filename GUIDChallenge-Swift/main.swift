fbd0ab68226b0b7a9718905cc7793a2e0dd4920c
James Howard <jameshoward@mac.com>
2017-02-07 10:17:52 -0800

    First commit

--- GUIDChallenge-Swift/main.swift
+++ GUIDChallenge-Swift/main.swift
@@ -8,5 +8,18 @@
 
 import Foundation
 
-print("Hello, World!")
+let limit = 10000000;
+let start = CFAbsoluteTimeGetCurrent();
+
+let uuid = UnsafeMutablePointer<UInt8>.allocate(capacity: 16)
+for i in 0...limit {
+    uuid_generate(uuid)
+}
+
+let end = CFAbsoluteTimeGetCurrent();
+
+print("Done in \(end-start)")
+
+
+
 
